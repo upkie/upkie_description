c788bc25ab5beaa26726a4e30303f0935307cb1a
Stéphane Caron <stephane.caron@normalesup.org>
2023-03-14 15:32:05 +0100

    [python] Add load_in_pinocchio function

---
+++ dist/python/upkie_description/load_in_pinocchio.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2022 Stéphane Caron
+# Copyright 2023 Inria
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Load Upkie's description in Pinocchio."""
+
+import os
+from typing import Optional, Union
+
+import pinocchio as pin
+
+from .paths import path as package_path
+from .paths import urdf_path
+
+PinocchioJoint = Union[
+    pin.JointModelRX,
+    pin.JointModelRY,
+    pin.JointModelRZ,
+    pin.JointModelPX,
+    pin.JointModelPY,
+    pin.JointModelPZ,
+    pin.JointModelFreeFlyer,
+    pin.JointModelSpherical,
+    pin.JointModelSphericalZYX,
+    pin.JointModelPlanar,
+    pin.JointModelTranslation,
+]
+
+
+def load_in_pinocchio(
+    description_name: str,
+    root_joint: Optional[PinocchioJoint] = None,
+    commit: Optional[str] = None,
+) -> pin.RobotWrapper:
+    """Load a robot description in Pinocchio.
+
+    Args:
+        description_name: Name of the robot description.
+        root_joint (optional): First joint of the kinematic chain, for example
+            a free flyer between the floating base of a mobile robot and an
+            inertial frame. Defaults to no joint, i.e., a fixed base.
+        commit: If specified, check out that commit from the cloned robot
+            description repository.
+
+    Returns:
+        Robot model for Pinocchio.
+    """
+    robot = pin.RobotWrapper.BuildFromURDF(
+        filename=urdf_path,
+        package_dirs=[package_path, os.path.dirname(package_path)],
+        root_joint=root_joint,
+    )
+    return robot
