915f39bb8c041f719a36253d1ebdf1ebaf35a466
Stéphane Caron <stephane.caron@inria.fr>
2025-10-15 07:05:12 +0200

    Update display_collisions example

--- examples/display_collisions.py
+++ examples/display_collisions.py
@@ -3,14 +3,19 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 # Copyright 2024 Inria
+#
+# /// script
+# dependencies = ["pin", "upkie_description"]
+# ///
 
 """Display the robot description along and its collision meshes."""
 
 import argparse
 import time
 
-import upkie_description
 from pinocchio.visualize import MeshcatVisualizer
+
+import upkie_description
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description=__doc__)
@@ -27,4 +32,10 @@ if __name__ == "__main__":
     robot.viz.displayCollisions(True)
     robot.display(robot.q0)
 
-    time.sleep(1.0)  # avoid terminating too fast
+    print("Press Ctrl-C to exit.")
+    t = 0.0
+    dt = 1.0  # seconds
+    while True:
+        robot.display(robot.q0)
+        time.sleep(dt)
+        t += dt
